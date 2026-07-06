88e39755dc24f3a30d8832537d453132acc2c637
Adela015 <ade.cervantes01@gmail.com>
2026-07-06 20:19:04 -0300

    practica de importaciones

---
+++ modules-ejecicio/datos.py
@@ -0,0 +1,7 @@
+import base_datos as db
+
+clientes = db.base_datos["clientes"]
+empleados = db.base_datos["empleados"]
+
+print(f"Clientes: {db.base_datos['clientes']} \n")
+print(f"Empleados: {db.base_datos['empleados']} \n")
---
+++ modules/import-2.py
@@ -0,0 +1,6 @@
+from operaciones import sumar, restar, multiplicar, dividir
+
+dividir(10, 2)
+sumar(5, 3)
+restar(10, 4)
+multiplicar(6, 7)
